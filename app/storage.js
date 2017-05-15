f189084f2eae38d71ff6bc3d4ff082a2d6496c7e
MGazizov <gazizovmarat@gmail.com>
2017-05-15 13:51:20 +0300

    added subscribers to storage, linked charts to storage

--- app/storage.js
+++ app/storage.js
@@ -5,21 +5,30 @@ export let Storage = (function () {
                 photoUrl: ''    
               }
     };
-
+    const listeners = [];
 	class StorageInner {
         constructor() {
             this.set = this.set.bind(this);
             this.get = this.get.bind(this);
+            this.dispatch = this.dispatch.bind(this);
+            this.subscribe = this.subscribe.bind(this);
         }
 
         set(itemName, newVal) {
             console.log('Past local store', store);
             store[itemName] = newVal;
             console.log('New local store', store);
+            this.dispatch();
         }
         
         get(itemName) {
             return store[itemName];
+        }
+         dispatch(){
+            listeners.forEach(listener => listener());
+        }
+        subscribe(listener){
+            listeners.push(listener);
         }
     }
     
